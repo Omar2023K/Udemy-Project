9cc6aa0891570b6fed00398443e02e12a044cfb0
Omar2023K <147134657+Omar2023K@users.noreply.github.com>
2024-08-30 16:35:09 +0300

    Update streamlit_udemy.py

--- streamlit_udemy.py
+++ streamlit_udemy.py
@@ -15,9 +15,9 @@ st.set_page_config(
 )
 df = pd.read_csv("udemy_data_clean.csv")
 st.sidebar.header('Udemy Dashboard Filter')
-st.sidebar.write('This dataset contain information about online courses, from a platform like Udemy.')
-
 st.sidebar.image('Udemy.jpg')
+
+st.sidebar.write('This dataset contain information about online courses, from a platform like Udemy.')
 years = df['year'].unique().tolist()
 years.insert(0, 'All')
 selected_year = st.sidebar.selectbox('Select Year', options=years)
